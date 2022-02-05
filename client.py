c14192eba79dbfcd94dd5b39d73c1a2bd064bc34
Andre Fernandes <fpf.andre@gmail.com>
2022-02-04 23:05:26 -0300

    add my python test client because why not

---
+++ client.py
@@ -0,0 +1,13 @@
+from socket import socket, AF_INET, SOCK_STREAM
+
+HOST = '127.0.0.1'
+PORT = 1235
+
+with socket(AF_INET, SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    
+    while True:
+        message = input()
+        if not message:
+            break
+        s.sendall(bytes(message, 'utf-8'))
